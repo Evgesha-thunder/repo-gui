5402903993ee6d27438e15636384c25e2b9533ce
Evgenia <Bulish2015@yandex.ru>
2020-11-13 14:00:08 +0300

    commit 02

--- new-feature.go
+++ new-feature.go
@@ -1,9 +1,13 @@
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 some content
 =======
 change new-feture.go
 >>>>>>> Stashed changes
 =======
 trtshs
+>>>>>>> Stashed changes
+=======
+change new-feture.go
 >>>>>>> Stashed changes
